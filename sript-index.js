7767c6dab3a8b5b64d83768b1c35a2b5e2412cfc
lhiemzethan <lhiemzethan@gmail.com>
2025-03-09 17:59:18 +0700

    Update sript-index.js

--- sript-index.js
+++ sript-index.js
@@ -2,6 +2,5 @@
 const btnlogin = document.querySelector('.popup-login');
 
 btnlogin.addEventListener('click', (event) => {
-    event.preventDefault();
-    window.location.href = "login.html";
+    window.location.assign("login.html");
 });
